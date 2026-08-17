51dfc998f1ba31ac0262b25147004764e352e9b7
cdelima-hue <cdelima@thestoreintelligence.com>
2026-08-17 12:20:20 -0600

    Delete Abrir_Portal_Changes.bat

--- Abrir_Portal_Changes.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Iniciar Portal de Gestion de Changes - Nestle
-echo ========================================================
-echo   Iniciando Portal de Gestion de Changes y Capacidad
-echo ========================================================
-echo.
-echo Abriendo en su navegador web predeterminado...
-start "" "%~dp0index.html"
-exit
